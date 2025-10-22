cf61de2b3d3e26aa44b02d41aa29de1594325a1e
Nguyen Phu An <nguyenanhp04@gmail.com>
2025-10-22 21:18:21 +0700

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,165 @@
+import bell_icon from './bell.png'
+import home_icon from './home.png'
+import like_icon from './like.png'
+import loop_icon from './loop.png'
+import mic_icon from './mic.png'
+import next_icon from './next.png'
+import play_icon from './play.png'
+import pause_icon from './pause.png'
+import plays_icon from './plays.png'
+import prev_icon from './prev.png'
+import search_icon from './search.png'
+import shuffle_icon from './shuffle.png'
+import speaker_icon from './speaker.png'
+import stack_icon from './stack.png'
+import zoom_icon from './zoom.png'
+import plus_icon from './plus.png'
+import arrow_icon from './arrow.png'
+import mini_player_icon from './mini-player.png'
+import queue_icon from './queue.png'
+import volume_icon from './volume.png'
+import arrow_right from './right_arrow.png'
+import arrow_left from './left_arrow.png'
+import spotify_logo from './spotify_logo.png'
+import clock_icon from './clock_icon.png'
+import img2 from './img2.jpg'
+import song2 from './song2.mp3'
+
+export const assets = {
+    bell_icon,
+    home_icon,
+    like_icon,
+    loop_icon,
+    mic_icon,
+    next_icon,
+    play_icon,
+    plays_icon,
+    prev_icon,
+    search_icon,
+    shuffle_icon,
+    speaker_icon,
+    stack_icon,
+    zoom_icon,
+    plus_icon,
+    arrow_icon,
+    mini_player_icon,
+    volume_icon,
+    queue_icon,
+    pause_icon,
+    arrow_left,
+    arrow_right,
+    spotify_logo,
+    clock_icon
+}
+
+export const albumsData = [
+    {   
+        id:0,
+        name: "Top 50 Global",
+        image: img2,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#2a4365"
+    },
+    {   
+        id:1,
+        name: "Top 50 Vietnam",
+        image: img2,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#22543d"
+    },
+    {   
+        id:2,
+        name: "Trending Vietnam",
+        image: img2,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#742a2a"
+    },
+    {   
+        id:3,
+        name: "Trending Global",
+        image: img2,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#44337a"
+    },
+    {   
+        id:4,
+        name: "Lạc trôi",
+        image: img2,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#234e52"
+    },
+    {   
+        id:5,
+        name: "Happy Favorites",
+        image: img2,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#744210"
+    }
+]
+
+export const songsData = [
+    {
+        id:0,
+        name: "Nơi này có anh",
+        image: img2,
+        file:song2,
+        desc:"goat",
+        duration:"3:53"
+    },
+    {
+        id:1,
+        name: "Nơi này có anh",
+        image: img2,
+        file:song2,
+        desc:"goat",
+        duration:"3:53"
+    },
+    {
+        id:2,
+        name: "Nơi này có anh",
+        image: img2,
+        file:song2,
+        desc:"goat",
+        duration:"3:53"
+    },
+    {
+        id:3,
+        name: "Nơi này có anh",
+        image: img2,
+        file:song2,
+        desc:"goat",
+        duration:"3:53"
+    },
+    {
+        id:4,
+        name: "Nơi này có anh",
+        image: img2,
+        file:song2,
+        desc:"goat",
+        duration:"3:53"
+    },
+    {
+        id:5,
+        name: "Nơi này có anh",
+        image: img2,
+        file:song2,
+        desc:"goat",
+        duration:"3:53"
+    },
+    {
+        id:6,
+        name: "Nơi này có anh",
+        image: img2,
+        file:song2,
+        desc:"goat",
+        duration:"3:53"
+    },
+    {
+        id:7,
+        name: "Nơi này có anh",
+        image: img2,
+        file:song2,
+        desc:"goat",
+        duration:"3:53"
+    }
+]
